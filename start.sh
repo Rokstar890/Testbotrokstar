f41c1241bf567ef95e0472bc4ad9dbd61441ba78
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-10 09:47:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/UnknownBotz/EasyAdvAutoFilter.git /EasyAdvAutoFilter
+  git clone https://github.com/Rokstar890/Testbotrokstar.git /Testbotrokstar
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EasyAdvAutoFilter
+  git clone $UPSTREAM_REPO /Testbotrokstar
 fi
-cd /EasyAdvAutoFilter
+cd /Testbotrokstar
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
